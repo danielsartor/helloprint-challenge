4f53db414da3792d0c95a20d38b34ec46eca477b
Daniel Sartor <danields28@gmail.com>
2020-07-28 15:31:40 +0100

    Added Postgres and DB creation

---
+++ docker/postgres/helloprint.sql
@@ -0,0 +1,13 @@
+-- Create the schema that we'll use to populate data and watch the effect in the binlog
+CREATE SCHEMA IF NOT EXISTS helloprint AUTHORIZATION hellouser;
+SET search_path TO helloprint;
+
+-- enable PostGis 
+CREATE EXTENSION postgis;
+
+-- Create table
+CREATE TABLE requests (
+  id SERIAL NOT NULL PRIMARY KEY,
+  message VARCHAR(255) NOT NULL,
+  response VARCHAR(255) NULL
+);
