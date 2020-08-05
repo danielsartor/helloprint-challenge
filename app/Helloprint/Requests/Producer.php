7545d7989104f02b853d21d8cd75e7324e1c637c
Daniel Sartor <danields28@gmail.com>
2020-08-05 17:16:10 +0100

    Remove some verifications

--- app/Helloprint/Requests/Producer.php
+++ app/Helloprint/Requests/Producer.php
@@ -20,21 +20,13 @@ class Producer
         $this->producer = new \RdKafka\Producer($this->configKafka);
 
         //Broker
-        if ($this->producer->addBrokers($this->brokerKafka) < 1) {
-            echo "Failed adding brokers\n";
-            exit;
-        }
+        $this->producer->addBrokers($this->brokerKafka);
     }
 
     public function sendMessageToTopic($message)
     {
         $this->topic = $this->producer->newTopic($this->topicName);
 
-        if (!$this->producer->getMetadata(false, $this->topic, 2000)) {
-            echo "Failed to get metadata, is broker down?\n";
-            exit;
-        }
-        
         $this->topic->produce(RD_KAFKA_PARTITION_UA, 0, $message);
 
         echo "Produced message $message for Topic $this->topicName \n\n";
